d0be9ea6d49190acbd6e3c4ad49edf70c9af81eb
luckyhyom <bs_khm@naver.com>
2021-06-03 23:53:46 +0900

    loop..

--- index.php
+++ index.php
@@ -1,7 +1,39 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    
+    $list = scandir('./data');
+    
+$a = 0;
 
-echo 2+2;
-echo $_SERVER['HTTP_HOST'];
-echo $_SERVER['REMOTE_ADDR'];
-echo $_GET['name'];
-?>
+    while ($a <= count($list)) {
+        # code...
+        echo "<li><a href=\"index.php?id=$list[$a]\">$list[$a]</a></li>";
+        $a +=1;
+    }
+
+    
+
+    ?>
+
+    <h2>
+    <?php 
+    
+    if(isset($_GET['id'])){
+        echo $_GET['id'];
+    }else{
+        echo "WELCOME!";
+    }
+
+
+    ?>
+    </h2>
+</body>
+</html>
